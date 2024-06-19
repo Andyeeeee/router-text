e02f252509366161bd2bbb511835e162815b7809
Andyeeeee <10514f074@gmail.com>
2024-06-19 16:30:26 +0800

    first commit

---
+++ src/views/admin/ProductsView.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h1>請選擇管理項目adminproject</h1>
+      </v-col>
+    </v-row>
+  </v-container>
+  <!-- <VContainer class="h-100">
+    <VRow class="h-100 align-center">
+      <VCol cols="12">
+        <h1 class="text-center">請選擇管理項目</h1>
+      </VCol>
+    </VRow>
+  </VContainer> -->
+</template>
